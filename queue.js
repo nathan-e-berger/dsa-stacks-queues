5a239043c3d24b6ed1a36e629f201bcfa880709d
Nathan Berger <nberger101@gmail.com>
2023-11-06 18:07:04 -0500

    added queue methods

--- queue.js
+++ queue.js
@@ -20,26 +20,45 @@ class Queue {
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
   enqueue(val) {
+    let newNode = new Node(val);
 
+    if (this.size === 0) {
+      this.first = newNode;
+      this.last = newNode;
+      this.size += 1;
+      return;
+    }
+
+    this.last.next = newNode;
+    this.last = newNode;
+    this.size += 1;
+    return;
   }
 
   /** dequeue(): remove the node from the start of the queue
    * and return its value. Should throw an error if the queue is empty. */
 
   dequeue() {
+    if (this.size === 0) throw new Error("thats illegal");
 
+    let item = this.first.val;
+    this.first.next = this.first;
+    this.size -= 1;
+    return item;
   }
 
   /** peek(): return the value of the first node in the queue. */
+  //no test for if queue is empty
 
   peek() {
-
+    return this.first.val;
   }
 
   /** isEmpty(): return true if the queue is empty, otherwise false */
 
   isEmpty() {
-
+    if (this.size === 0) return true;
+    return false;
   }
 }
 
